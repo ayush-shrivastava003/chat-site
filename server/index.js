f1b306c2e1b454a0a9da31049002b20fe12cb8bc
moistpotato9873 <77131808+ayush-shrivastava003@users.noreply.github.com>
2021-10-24 01:31:29 -0400

    live messaging!

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 import express from 'express'
-import rateLimit from 'express-rate-limit'
-import fs from 'fs'
 import mongoose from 'mongoose'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
@@ -8,6 +6,8 @@ import path from 'path'
 import {logReq, logCustom, logErr, getDate} from './logger.js'
 import verifyLogin from './login_checker.js'
 import MessageModel from './models/message_model.js'
+import http from 'http'
+import {Server} from 'socket.io'
 
 dotenv.config()
 mongoose.connect(
@@ -28,36 +28,36 @@ mongoose.connect(
 )
 
 const server = express()
+const HTTPServer = http.Server(server)
+const socket = new Server(HTTPServer)
+const router = express.Router();
 
 server.set('view engine', 'ejs')
 server.use('/assets', express.static(path.resolve() + '../assets'))
 server.use(cookieParser())
 server.use(express.json())
 server.use(logReq)
+server.use('/', router)
 
-server.listen(process.env.PORT, () => {
-    console.log(`Listening on port: ${process.env.PORT}.`)
-    logCustom(`Listening on port: ${process.env.PORT}.`)
+socket.on("connection", (socket) => {
+    console.log("accepted connection. id:", socket.id)
+    socket.on("disconnect", () => {
+        console.log("lost connection") 
+    })
+
+    socket.on("message", async (msg) => {
+        let message = new MessageModel({content: msg})
+        await message.save()
+
+        socket.broadcast.emit("new", {message: message})
+    })
 })
 
-server.get('/', async (req, res) => {
-    res.render('index')
+router.get('/', async (req, res) => {
+    let messages = await MessageModel.find().limit(25)
+    res.render('index', {messages: messages})
 })
 
-server.post('/post', async (req, res) => {
-    try {
-        let msg = req.body.msg
-        let newMsg = new MessageModel({
-            content: msg,
-        })
-        await newMsg.save()
-        logCustom(`Message received: ${msg}`)
-        return res.json({status: 'ok'})
-    } catch (err) {
-        return res.json({status: 'error', error: err.toString()})
-    }
+HTTPServer.listen(process.env.PORT, ()=> {
+    console.log("Started HTTP server. Port:", process.env.PORT)
 })
-
-// server.get("/home", verifyLogin, async (req, res) => {
-//     res.render("index")
-// })
