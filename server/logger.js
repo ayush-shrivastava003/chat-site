b778102a017b8913f1e72772f276c6e7421e41be
Ayush Shrivastava <77131808+ayush-shrivastava003@users.noreply.github.com>
2022-01-22 23:06:02 -0500

    Update logger.js

--- server/logger.js
+++ server/logger.js
@@ -6,13 +6,13 @@ function getDate() {
     let month = d.getMonth() + 1
     let year = d.getFullYear()
 
-    return `${month}/${day}/${year} @ ${new Date().toTimeString()}`
+    return `${month}/${day}/${year}`
 }
 
 function logReq(req, res, next) {
     fs.appendFile(
         'logs.log',
-        `${getDate()}: ${req.connection.remoteAddress} made a ${
+        `${getDate()} @ ${new Date().toTimeString()}: ${req.connection.remoteAddress} made a ${
             req.method
         } request for ${req.originalUrl}\n`,
         () => {}
@@ -21,11 +21,11 @@ function logReq(req, res, next) {
 }
 
 function logCustom(msg) {
-    fs.appendFile('logs.log', `${getDate()}: ${msg}\n`, () => {})
+    fs.appendFile('logs.log', `${getDate()} @ ${new Date().toTimeString()}: ${msg}\n`, () => {})
 }
 
 function logErr(msg) {
-    fs.appendFile('logs.log', `ERROR: ${getDate()}: ${msg}\n`, () => {})
+    fs.appendFile('logs.log', `ERROR: ${getDate()} @ ${new Date().toTimeString()}: ${msg}\n`, () => {})
 }
 
 export {logCustom, logReq, logErr, getDate}
