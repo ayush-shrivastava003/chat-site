ebb67762f073f04fb4b367ccabd1a601575782c9
Tristan <tschartman41@gmail.com>
2022-01-28 14:29:50 -0500

    Update room.js

--- assets/js/room.js
+++ assets/js/room.js
@@ -44,56 +44,56 @@ function resize(keyCode) {
 
 resize(13)
 
-// entry.addEventListener('keyup', (event) => {
-//     if (event.keyCode == 13 && entry.value != '') {
-//         if (entry.value.length > 2000) {
-//             alert("message content is more than 2000 characters")
-//             return
-//         }
-//         let roomId = window.location.href.split('/')[4]
-//         socket.emit("message", {content: entry.value, roomId: roomId, author: getUser()})
-//         append(entry.value, getUser())
-//         entry.value = ''
-//         msgContainer.scrollTop = msgContainer.scrollHeight;
-//     } else {
-//         setTimeout(() => {
-//             socket.emit("stop typing", getUser())
-//         }, 5000)
-//         socket.emit("typing", getUser())
-//     }
-// })
-
-let sent_typing = false;
-
-entry.addEventListener("keyup", (e) => {
-    if (e.code.toString() == "Enter") {
-        if (entry.value.length === 0 || entry.value.split(" ").join("") === "") {
-            return;
-        }
-        let id = window.location.pathname.split("/");
-        id = id[id.length-1];
-        socket.emit("message", {content:entry.value, roomId:id, author:getUser()});
-        socket.emit("stop typing", getUser());
-        sent_typing = false;
-        append();
-        return;
-    }
-    if (entry.value.length > 0) {
-        if (!sent_typing) {
-            sent_typing = true;
-            socket.emit("typing", getUser());
-        }
+entry.addEventListener('keyup', (event) => {
+    if (event.keyCode == 13 && entry.value != '') {
         if (entry.value.length > 2000) {
-            entry.value = entry.value.slice(0, 2000);
-            return;
+            alert("message content is more than 2000 characters")
+            return
         }
+        let roomId = window.location.href.split('/')[4]
+        socket.emit("message", {content: entry.value, roomId: roomId, author: getUser()})
+        append(entry.value, getUser())
+        entry.value = ''
+        msgContainer.scrollTop = msgContainer.scrollHeight;
     } else {
-        if (sent_typing) {
-            sent_typing = false;
-            socket.emit("stop typing", getUser());
-        }
+        setTimeout(() => {
+            socket.emit("stop typing", getUser())
+        }, 5000)
+        socket.emit("typing", getUser())
     }
-});
+})
+
+// let sent_typing = false;
+
+// entry.addEventListener("keyup", (e) => {
+//     if (e.code.toString() == "Enter") {
+//         if (entry.value.length === 0 || entry.value.split(" ").join("") === "") {
+//             return;
+//         }
+//         let id = window.location.pathname.split("/");
+//         id = id[id.length-1];
+//         socket.emit("message", {content:entry.value, roomId:id, author:getUser()});
+//         socket.emit("stop typing", getUser());
+//         sent_typing = false;
+//         append();
+//         return;
+//     }
+//     if (entry.value.length > 0) {
+//         if (!sent_typing) {
+//             sent_typing = true;
+//             socket.emit("typing", getUser());
+//         }
+//         if (entry.value.length > 2000) {
+//             entry.value = entry.value.slice(0, 2000);
+//             return;
+//         }
+//     } else {
+//         if (sent_typing) {
+//             sent_typing = false;
+//             socket.emit("stop typing", getUser());
+//         }
+//     }
+// });
 
 
 document.getElementById("file-upload").addEventListener("click", () => {
