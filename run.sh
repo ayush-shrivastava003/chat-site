10a882e2d3cf9f277b8a60a78aba8eab009a2f03
ayush-shrivastava003 <77131808+ayush-shrivastava003@users.noreply.github.com>
2022-02-06 17:17:53 -0500

    i have two brain cells and both are dead

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #! /bin/bash
 
-# echo "PORT=3000
-# DB=mongodb://$(hostname -I):27017/chat-site
-# JWT_SECRET=LT9YbkomX34xWjQ6cyxRTlFcNnBrckhtM2d6UHlBT1poS15vdy47Q3pGPjFXKUxSSjdoYEwlQS4nLyk6XlBnZVRJPEVgZ0t5" > .env
-
 source $HOME/.bashrc && npm run dev
